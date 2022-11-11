b62433a01faf69db6f38194afb732bd3061ce6a7
Durgesh chaubey <durgesh.chaubey@bharatnxt.in>
2022-11-11 12:02:45 +0530

    Completed the basic functionalities

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var operand1=0;
 var operand2=null;
 var result=null;
+var operator=null;
 
 
 
@@ -11,12 +12,71 @@ for(let i=0;i<btn.length;i++){
     btn[i].addEventListener('click',function(){
         if(btn[i].innerText=="="){
             var display=document.getElementById("value");
+            operand2=display.innerText;
+            display.innerText="";
+           
+            result=eval(`${operand1}${operator}${operand2}`);
+            console.log(typeof(result));
+            if(isNaN(result) || !isFinite(result)){
+                display.style.color="Red";
+                display.innerText="Error !";
+                operand1=null;
+                operand2=null;
+                operator=null
+            }
+            else{
+            display.innerText=result;
+            operand1=result;
+        }
+            }
+        
+        else if(btn[i].innerText=="+"){
+            var display=document.getElementById("value");
             operand1=display.innerText;
             display.innerText="";
-            display.innerText+=btn[i].innerText;
-            display.innerText="";
-            console.log("equals find");
+            operator=btn[i].innerText;
         }
+        else if(btn[i].innerText=="-"){
+            var display=document.getElementById("value");
+            operand1=display.innerText;
+            display.innerText="";
+            operator=btn[i].innerText;
+        }
+        else if(btn[i].innerText=="/"){
+            var display=document.getElementById("value");
+            operand1=display.innerText;
+            display.innerText="";
+            operator=btn[i].innerText;
+        }
+        else if(btn[i].innerText=="*"){
+            var display=document.getElementById("value");
+            operand1=display.innerText;
+            display.innerText="";
+            operator=btn[i].innerText;
+        }
+        else if(btn[i].innerText=="%"){
+            var display=document.getElementById("value");
+            operand1=display.innerText;
+            display.innerText="";
+            operator=btn[i].innerText;
+        }
+        else if(btn[i].innerText=="+/-"){
+            var display=document.getElementById("value");
+            operand1=display.innerText;
+operand1=operand1*-1;
+            display.innerText=operand1;
+            
+        }
+        else if(btn[i].innerText=="AC"){
+            var display=document.getElementById("value");
+            display.innerText="";
+            operand1=0;
+            operand2=null;
+            operator=null;
+        }
+
+      
+      
         else{
         var display=document.getElementById("value");
         display.innerText+=btn[i].innerText;
