e78e5b5c60404298ea6acb60ce8cc431bf2f0b55
Durgesh chaubey <durgesh.chaubey@bharatnxt.in>
2022-11-11 12:28:31 +0530

    added all the functionalities to perform the operation by using the click event and using the keyboard event

--- index.js
+++ index.js
@@ -4,6 +4,68 @@ var result=null;
 var operator=null;
 
 
+document.addEventListener('keypress',function(event){
+    console.log(event.keyCode);
+    if(event.keyCode==61 || event.keyCode==13){
+        var display=document.getElementById("value");
+        operand2=display.innerText;
+        display.innerText="";
+       
+        result=eval(`${operand1}${operator}${operand2}`);
+        console.log(typeof(result));
+        if(isNaN(result) || !isFinite(result)){
+            display.style.color="Red";
+            display.innerText="Error !";
+            operand1=null;
+            operand2=null;
+            operator=null
+        }
+        else{
+        display.innerText=result;
+        operand1=result;
+    }
+        }
+    
+    else if(event.keyCode==43){
+        var display=document.getElementById("value");
+        operand1=display.innerText;
+        display.innerText="";
+        operator=String.fromCharCode(event.keyCode);
+    }
+    else if(event.keyCode==45){
+        var display=document.getElementById("value");
+        operand1=display.innerText;
+        display.innerText="";
+        operator=String.fromCharCode(event.keyCode);
+    }
+    else if(event.keyCode==47){
+        var display=document.getElementById("value");
+        operand1=display.innerText;
+        display.innerText="";
+        operator=String.fromCharCode(event.keyCode);
+    }
+    else if(event.keyCode==42){
+        var display=document.getElementById("value");
+        operand1=display.innerText;
+        display.innerText="";
+        operator=String.fromCharCode(event.keyCode);
+    }
+    else if(event.keyCode==37){
+        var display=document.getElementById("value");
+        operand1=display.innerText;
+        display.innerText="";
+        operator=String.fromCharCode(event.keyCode);
+    }
+   
+   
+     else{
+        if(event.keyCode>=48 && event.keyCode<=57){
+    var display=document.getElementById("value");
+    display.innerText+=String.fromCharCode(event.keyCode);
+        }
+    }   
+
+})
 
 var btn=document.getElementsByClassName("button");
 console.log(btn);
@@ -74,10 +136,7 @@ operand1=operand1*-1;
             operand2=null;
             operator=null;
         }
-
-      
-      
-        else{
+         else{
         var display=document.getElementById("value");
         display.innerText+=btn[i].innerText;
         }   
